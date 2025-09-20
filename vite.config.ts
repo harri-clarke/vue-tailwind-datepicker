871f57393536460384dcbe336866a3f87ca6c184
Harri Clarke <clarkey08@live.co.uk>
2025-09-20 17:41:39 +0100

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import dts from "vite-plugin-dts";
+import path from "node:path"
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
+import dts from "vite-plugin-dts"
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    cssInjectedByJsPlugin(),
+    dts({
+      rollupTypes: true,
+      insertTypesEntry: true, // generates "types" entry in package.json
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/entry.ts"),
       name: "VueTailwindDatepicker",
-      fileName: "vue-tailwind-datepicker",
+      fileName: (format) => `vue-tailwind-datepicker.${format}.js`,
+      formats: ["es", "umd"],
     },
     rollupOptions: {
       external: ["vue"],
       output: {
-        // Provide global variables to use in the UMD build
-        // Add external deps here
         globals: {
           vue: "Vue",
         },
       },
     },
   },
-  plugins: [vue(), cssInjectedByJsPlugin(), dts({ rollupTypes: true })],
 })
